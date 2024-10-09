0e9461a57d3d2c4a8059e46b2ada126470faea9e
Ida Krech <krech.ida@gmail.com>
2024-10-09 14:18:02 +0200

    Wrap API service in IIFE module

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,19 +1,33 @@
 import axios from "axios"
 import buildURL from "../helpers/buildURL"
-import { FetchProductParams, Product } from "../types/interfaces"
+import { FetchProductParams, IProduct } from "../types/interfaces"
 
-const BASE_URL = "http://makeup-api.herokuapp.com/api/v1/products.json"
 
-export async function fetchProducts(
-  params: FetchProductParams
-): Promise<Product[]> {
-  const url = buildURL(BASE_URL, params)
 
-  try {
-    const response = await axios.get(url)
-    return response.data
-  } catch (error) {
-    console.error("Error fetching products:", error)
-    throw error
+const APIService = (
+  () => {
+    const BASE_URL = "http://makeup-api.herokuapp.com/api/v1/products.json"
+
+    async function fetchProducts(
+      params: FetchProductParams
+    ): Promise<IProduct[]> {
+      const url = buildURL(BASE_URL, params)
+    
+      try {
+        const response = await axios.get(url)
+        return response.data
+      } catch (error) {
+        console.error("Error fetching products:", error)
+        throw error
+      }
+    }
+
+    return {
+      fetchProducts
+    }
   }
-}
+)()
+
+export default APIService
+
+
