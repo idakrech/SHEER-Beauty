dd827f9fda37956dcaf9df7a4d16ab14b480e2d5
Ida Krech <krech.ida@gmail.com>
2024-10-09 14:33:12 +0200

    Implement helper function to validate image URL

---
+++ src/helpers/checkImage.ts
@@ -0,0 +1,20 @@
+const checkIfImageExists = (url: string, callback: (exists: boolean) => void) => {
+    const img = new Image()
+    img.src = url
+  
+    if (img.complete) {
+      callback(true)
+    } else {
+      img.onload = () => {
+        callback(true);
+      }
+  
+      img.onerror = () => {
+        callback(false)
+      }
+    }
+  }
+  
+  export default checkIfImageExists;
+
+  // credits: https://github.com/Kallil-Belmonte/helpers/blob/main/functions/image/checkIfImageExists.ts
