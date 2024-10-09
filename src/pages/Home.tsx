43a3dc3ded37c5a3370368fa705933b475ab1b47
Ida Krech <krech.ida@gmail.com>
2024-10-09 14:31:08 +0200

    Fetch and render products from app state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,29 @@
 import React from "react";
+import ProductGrid from "../components/ProductGrid";
 import useFetchProducts from "../hooks/useFetchProducts";
+import { useSelector } from "react-redux";
+import { AppState } from "../redux";
 
 const Home = () => {
-  const products = useFetchProducts({ brand: "maybelline" }).products;
+  
+  useFetchProducts({ product_tags: "Organic"})
+  useFetchProducts({ rating_greater_than: 4})
+  useFetchProducts({product_category: "lip_gloss"})
+
+  const organicProducts = useSelector((state: AppState) => state.products.products.filter(product => product.tag_list.filter(tag => tag == 'Organic')))
+  const highestRatedProducts = useSelector((state: AppState) => state.products.products.filter(product => ((product.rating && product.rating >= 4))))
+  const lipglossProducts = useSelector((state: AppState) => state.products.products.filter(product => ((product.category && product.category == 'lip_gloss'))))
 
   return (
     <>
-      <div>Home</div>
-      {products.map((product) => (
-        <div>
-          <p>{product.name}</p>
-          <p>{product.brand}</p>
-          <p>{product.image_link}</p>
-          <img src={product.image_link} />
-        </div>
-      ))}
+      <h1>Organic Products</h1>
+      <ProductGrid products={organicProducts}/>
+      <h1>Highest rated</h1>
+      <ProductGrid products={highestRatedProducts}/>
+      <h1>Time to shine!</h1>
+      <ProductGrid products={lipglossProducts}/>
     </>
-  );
-};
+  )
+}
 
 export default Home;
