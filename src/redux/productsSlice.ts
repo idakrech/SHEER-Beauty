09b7a5fe809d607ff46fab70e32505bbf5d1b9e0
Ida Krech <krech.ida@gmail.com>
2024-10-10 14:26:43 +0200

    Fix state update to avoid duplicate products

--- src/redux/productsSlice.ts
+++ src/redux/productsSlice.ts
@@ -2,33 +2,40 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IProduct } from "../types/interfaces";
 
 export interface ProductsState {
-    products: IProduct[],
-    loading: boolean,
-    error: Error | null
+  products: IProduct[];
+  loading: boolean;
+  error: Error | null;
 }
 
 export const initialState: ProductsState = {
-    products: [],
-    loading: false,
-    error: null
-}
+  products: [],
+  loading: false,
+  error: null,
+};
 export const productsSlice = createSlice({
-    name: 'products',
-    initialState,
-    reducers: {
-        setProducts(state, action: PayloadAction<IProduct[]>) {
-            state.products = action.payload
-            state.loading = false
-            state.error = null
-        },
-        setError(state, action: PayloadAction<Error>) {
-            state.error = action.payload
-            state.loading = false 
-        },
-        setLoading(state, action: PayloadAction<boolean>) {
-            state.loading = action.payload
-        }
-    }
-})
+  name: "products",
+  initialState,
+  reducers: {
+    setProducts(state, action: PayloadAction<IProduct[]>) {
+      if (state.products.length > 0) {
+        const updatedProducts = state.products.filter(
+          (product) => !action.payload.some((p) => p.id === product.id)
+        );
+        state.products = [...updatedProducts, ...action.payload];
+      } else {
+        state.products = action.payload;
+      }
+      state.loading = false;
+      state.error = null;
+    },
+    setError(state, action: PayloadAction<Error>) {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    setLoading(state, action: PayloadAction<boolean>) {
+      state.loading = action.payload;
+    },
+  },
+});
 
-export const {setProducts, setError, setLoading } = productsSlice.actions
+export const { setProducts, setError, setLoading } = productsSlice.actions;
