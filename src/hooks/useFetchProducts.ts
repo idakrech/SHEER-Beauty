e90497658765255b9950785f4f12469b54184e55
Ida Krech <krech.ida@gmail.com>
2024-10-15 19:48:56 +0200

    Modify product filtering in grids: from param-based to ID-based

--- src/hooks/useFetchProducts.ts
+++ src/hooks/useFetchProducts.ts
@@ -1,21 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect } from "react";
-import { IFetchProductParams } from "../types/interfaces";
+import { IFetchProductParams, IProduct } from "../types/interfaces";
 import APIService from "../services/APIService";
 import { setError, setLoading, setProducts } from "../redux/productsSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { AppState } from "../redux";
+import { setProductIDs } from "../redux/filterSlice";
 
 function useFetchProducts(params: IFetchProductParams[]) {
 
     const dispatch = useDispatch()
+    const filters = useSelector((state: AppState) => state.filters)
 
     useEffect(() => {
         const fetchData = async() => {
             dispatch(setLoading(true))
 
             try {
-                params.forEach(async (p) => {
+                params.forEach(async (p, index) => {
                     const data = await APIService.fetchProducts(p)
                     dispatch(setProducts(data))
+                    const productIDs = data.map((product: IProduct) => product.id)
+                    const gridID = filters[index].gridID
+                    dispatch(setProductIDs({gridID, productIDs}))
                 })
                 
             } catch (error) {
--- src/redux/filterSlice.ts
+++ src/redux/filterSlice.ts
@@ -1,23 +1,54 @@
-import { createSlice} from "@reduxjs/toolkit";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IFetchProductParams } from "../types/interfaces";
 
 export interface IGridFilter {
-    gridID: string,
-    title: string,
-    fetchParams: IFetchProductParams,
-    createdAt?: string //necessary for new arrivals section
-  }
-  
-export type IFilterState = IGridFilter[]
+  gridID: string;
+  title: string;
+  fetchParams: IFetchProductParams;
+  productIDs: number[];
+  createdAt?: string; //necessary for new arrivals section
+}
+
+export type IFilterState = IGridFilter[];
 
 export const initialState: IFilterState = [
-    { gridID: "grid1", title: "Vegan products", fetchParams: { product_type: "eyeshadow", product_tags: ["Vegan"]}}, 
-    { gridID: "grid2", title: "Maybelline mascaras", fetchParams: {product_type: "Mascara", brand: "Maybelline"}, createdAt: "5/2/2015"},
-    { gridID: "grid3", title: "Highest rated Nyx products", fetchParams: {rating_greater_than: 4, brand: "nyx"}}
-]
+  {
+    gridID: "grid1",
+    title: "Vegan products",
+    fetchParams: { product_type: "eyeshadow", product_tags: ["Vegan"] },
+    productIDs: [],
+  },
+  {
+    gridID: "grid2",
+    title: "Maybelline mascaras",
+    fetchParams: { product_type: "Mascara", brand: "Maybelline" },
+    createdAt: "5/2/2014",
+    productIDs: [],
+  },
+  {
+    gridID: "grid3",
+    title: "Highest rated Nyx products",
+    fetchParams: { rating_greater_than: 4, brand: "nyx" },
+    productIDs: [],
+  },
+];
 
 export const filterSlice = createSlice({
-    name: "filters",
-    initialState,
-    reducers: {}
+  name: "filters",
+  initialState,
+  reducers: {
+    setProductIDs(
+      state,
+      action: PayloadAction<{ gridID: string; productIDs: number[] }>
+    ) {
+      const { gridID, productIDs } = action.payload;
+      const grid = state.find((g) => g.gridID === gridID);
+      if (grid) {
+        grid.productIDs = productIDs;
+      }
+    }
+  }
 })
+
+export const { setProductIDs } = filterSlice.actions
