665b050f0fee094a5b7df8aa5c8ad9dbb410b03c
Ida Krech <krech.ida@gmail.com>
2024-10-15 11:10:10 +0200

    Create mechanism for date-based product filtering

--- src/redux/filterSlice.ts
+++ src/redux/filterSlice.ts
@@ -1,18 +1,18 @@
 import { createSlice} from "@reduxjs/toolkit";
-import { IFetchProductParams, IProduct } from "../types/interfaces";
+import { IFetchProductParams } from "../types/interfaces";
 
 export interface IGridFilter {
     gridID: string,
     title: string,
     fetchParams: IFetchProductParams,
-    productParams?: IProduct //necessary for deeper-level product filtering
+    createdAt?: string //necessary for new arrivals section
   }
   
 export type IFilterState = IGridFilter[]
 
 export const initialState: IFilterState = [
     { gridID: "grid1", title: "Vegan products", fetchParams: { product_type: "eyeshadow", product_tags: ["Vegan"]}}, 
-    { gridID: "grid2", title: "Maybelline mascaras", fetchParams: {product_type: "Mascara", brand: "Maybelline"}},
+    { gridID: "grid2", title: "Maybelline mascaras", fetchParams: {product_type: "Mascara", brand: "Maybelline"}, createdAt: "5/2/2015"},
     { gridID: "grid3", title: "Highest rated Nyx products", fetchParams: {rating_greater_than: 4, brand: "nyx"}}
 ]
 
--- src/utils/filterProducts.tsx
+++ src/utils/filterProducts.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { AppState } from "../redux";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AppState } from "../redux"
 import ProductGrid from "../components/ProductGrid"
 
 export const filterProducts = (state: AppState) => {
@@ -8,19 +8,33 @@ export const filterProducts = (state: AppState) => {
   
     const productGrids = state.filters.map((grid) => {
 
+      const fetchParams = grid.fetchParams
+      const createdAtParam = grid.createdAt
+
       const filteredProducts = products.filter((product) => {
 
-        const params = grid.fetchParams
-  
-        const matchesCategory = params.product_category ? product.category?.toLowerCase() === params.product_category.toLowerCase() : true
-        const matchesTags = params.product_tags ? product.tag_list.filter((tag) => tag.toLowerCase()).some((t) => params.product_tags?.filter((pt) => pt.toLowerCase()).includes(t)) : true
-        const matchesType = params.product_type ? product.product_type.toLowerCase() === params.product_type.toLowerCase() : true
-        const matchesBrand = params.brand ? product.brand.toLowerCase() === params.brand.toLowerCase() : true
-        const matchesRatingGreaterThan = params.rating_greater_than ? product.rating != null && product.rating >= params.rating_greater_than : true
-        const matchesRatingLessThan = params.rating_less_than ? product.rating != null && product.rating <= params.rating_less_than : true
-        const matchesPriceGreaterThan = params.price_greater_than ? parseFloat(product.price) >= params.price_greater_than : true
-        const matchesPriceLessThan = params.price_less_than ? parseFloat(product.price) <= params.price_less_than : true
-  
+        const matchesCategory = fetchParams.product_category ? product.category?.toLowerCase() === fetchParams.product_category.toLowerCase() : true
+        const matchesTags = fetchParams.product_tags ? product.tag_list.filter((tag) => tag.toLowerCase()).some((t) => fetchParams.product_tags?.filter((pt) => pt.toLowerCase()).includes(t)) : true
+        const matchesType = fetchParams.product_type ? product.product_type.toLowerCase() === fetchParams.product_type.toLowerCase() : true
+        const matchesBrand = fetchParams.brand ? product.brand.toLowerCase() === fetchParams.brand.toLowerCase() : true
+        const matchesRatingGreaterThan = fetchParams.rating_greater_than ? product.rating != null && product.rating >= fetchParams.rating_greater_than : true
+        const matchesRatingLessThan = fetchParams.rating_less_than ? product.rating != null && product.rating <= fetchParams.rating_less_than : true
+        const matchesPriceGreaterThan = fetchParams.price_greater_than ? parseFloat(product.price) >= fetchParams.price_greater_than : true
+        const matchesPriceLessThan = fetchParams.price_less_than ? parseFloat(product.price) <= fetchParams.price_less_than : true
+
+        let isNewer: boolean = true
+
+        if (matchesCategory &&
+          matchesTags &&
+          matchesType &&
+          matchesBrand &&
+          matchesRatingGreaterThan &&
+          matchesRatingLessThan &&
+          matchesPriceGreaterThan &&
+          matchesPriceLessThan && createdAtParam) {
+            isNewer = new Date(createdAtParam).getTime() <= new Date(product.created_at).getTime()
+          }
+
         return (
           matchesCategory &&
           matchesTags &&
@@ -29,7 +43,8 @@ export const filterProducts = (state: AppState) => {
           matchesRatingGreaterThan &&
           matchesRatingLessThan &&
           matchesPriceGreaterThan &&
-          matchesPriceLessThan
+          matchesPriceLessThan &&
+          isNewer
         )
       })
 
