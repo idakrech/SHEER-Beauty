f75401a7628f224c2d894065f31925e3b947eb48
Ida Krech <krech.ida@gmail.com>
2024-10-18 13:05:03 +0200

    Add categories menu to Home page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import useFetchProducts from "../hooks/useFetchProducts";
-import { useSelector } from "react-redux";
-import { AppState } from "../redux";
-import { filterProducts } from "../utils/filterProducts";
+import useFetchProducts from "../hooks/useFetchProducts"
+import { useSelector } from "react-redux"
+import { AppState } from "../redux"
+import { filterProducts } from "../utils/filterProducts"
+import { useNavigate } from "react-router-dom"
+import NavigationList from "../components/NavigationList"
 
 const Home = () => {
   const state = useSelector((state: AppState) => state)
   const filterParams = state.filters.map((f) => f.fetchParams)
 
-  useFetchProducts(filterParams) 
+  useFetchProducts(filterParams)
 
-  return <>{filterProducts(state)}</>
+  return (
+    <>
+    <NavigationList/>
+      {filterProducts(state)}
+    </>
+  )
 }
 
 export default Home
