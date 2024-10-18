9c639fabfc50f2abb7f884abd5749d2b73b22f4a
Ida Krech <krech.ida@gmail.com>
2024-10-18 18:57:30 +0200

    Add logic to fetch and display cart products from state on Cart Page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,8 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { AppState } from '../redux'
+import ProductCard from '../components/ProductCard'
 
 const CartPage = () => {
+  const productIDs = useSelector((state: AppState) => state.cart.productIDs)
+  const products = useSelector((state: AppState) => state.products.products)
+  const cartProducts = products.filter((product) => productIDs.includes(product.id))
   return (
-    <div>CartPage</div>
+    <div>
+      {cartProducts.map((product) => <ProductCard {...product}/>)}
+      </div>
   )
 }
 
