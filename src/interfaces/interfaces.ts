a8dc445b42a42f392fd1c5978e5b887a12d787e7
Ida Krech <krech.ida@gmail.com>
2024-10-18 20:18:56 +0200

    Create universal interface for cart slice and favorites slice

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -40,4 +40,10 @@ export interface IProductType {
   name: string,
   categories: string[] | null
 }
+
+export interface IProductRefState {
+  productIDs: number []
+  loading: boolean
+  error: Error | null
+}
   
