7193b0b1e11059aba3957997d09070ebdfd7bf78
Ida Krech <krech.ida@gmail.com>
2024-10-18 20:45:09 +0200

    Add logic and buttons to add/remove product from favorites

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,21 +4,45 @@ import { IProduct } from "../interfaces/interfaces"
 import checkIfImageExists from "../helpers/checkImage"
 import PlaceholderImg from "../assets/placeholder.png"
 import { Link } from "react-router-dom"
-import { useDispatch } from "react-redux"
-import { addProductID } from "../redux/cartSlice"
+import { useDispatch, useSelector } from "react-redux"
+import { addProductID as addToCart } from "../redux/cartSlice"
+import {
+  addProductID as addToFavs,
+  deleteProductID as removeFromFavs,
+} from "../redux/favoritesSlice"
+import { AppState } from "../redux"
 
 const ProductCard = (props: IProduct) => {
   const [imgExists, setImgExists] = useState<boolean | null>(null)
+  const [isFavorite, setIsFavorite] = useState<boolean>()
+  const favProductIDs = useSelector(
+    (state: AppState) => state.favorites.productIDs
+  )
   const dispatch = useDispatch()
 
   useEffect(() => {
     checkIfImageExists(props.image_link, (exists: boolean) => {
       setImgExists(exists)
     })
+    setIsFavorite(checkIfFavorite)
   }, [])
-  
+
+  const checkIfFavorite = (): boolean => {
+    return favProductIDs.some((id) => props.id === id)
+  }
+
   const handleAddToCartBtn = () => {
-    dispatch(addProductID(props.id))
+    dispatch(addToCart(props.id))
+  }
+
+  const handleAddToFavsBtn = () => {
+    if (!isFavorite) {
+      dispatch(addToFavs(props.id))
+      setIsFavorite(true)
+    } else {
+      dispatch(removeFromFavs(props.id))
+      setIsFavorite(false)
+    }
   }
 
   return (
@@ -29,9 +53,8 @@ const ProductCard = (props: IProduct) => {
     //     <h3>{props.price}</h3>
     // </div>
     <Link to={`/product-page?id=${props.id}`}>
-    <div className="w-72 bg-white duration-500 hover:scale-105 border border-gray-300">
-      <a href="#">
-        
+      <div className="w-72 bg-white duration-500 hover:scale-105 border border-gray-300">
+        <a href="#">
           <div className="flex justify-center py-10">
             {imgExists === null ? (
               <p>Loading...</p>
@@ -41,46 +64,49 @@ const ProductCard = (props: IProduct) => {
               <img src={PlaceholderImg} />
             )}
           </div>
-        
-        <div className="px-4 py-3 w-72 bg-gray-100">
-          <span className="text-gray-400 mr-3 uppercase text-xs">
-            {props.brand}
-          </span>
-          <p className="text-lg font-bold text-black truncate block capitalize">
-            {props.name}
-          </p>
-          <div className="flex items-center">
-            <p className="text-lg font-semibold text-black cursor-auto my-3">
-              {props.price_sign ? props.price_sign : "$"}
-              {props.price}
-            </p>
-            <button onClick={() => handleAddToCartBtn()}>ADD</button>
 
-            {/* for old price */}
-            {/* <del>
+          <div className="px-4 py-3 w-72 bg-gray-100">
+            <span className="text-gray-400 mr-3 uppercase text-xs">
+              {props.brand}
+            </span>
+            <p className="text-lg font-bold text-black truncate block capitalize">
+              {props.name}
+            </p>
+            <div className="flex items-center">
+              <p className="text-lg font-semibold text-black cursor-auto my-3">
+                {props.price_sign ? props.price_sign : "$"}
+                {props.price}
+              </p>
+              <button onClick={() => handleAddToCartBtn()}>TO CART</button>
+              <button onClick={() => handleAddToFavsBtn()}>
+                {!isFavorite ? <p>ADD FAV</p> : <p>REMOVE FAV</p>}
+              </button>
+
+              {/* for old price */}
+              {/* <del>
                         <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
                     </del> */}
 
-            <div className="ml-auto">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="currentColor"
-                className="bi bi-bag-plus"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                />
-                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-              </svg>
+              <div className="ml-auto">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="bi bi-bag-plus"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                  />
+                  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                </svg>
+              </div>
             </div>
           </div>
-        </div>
-      </a>
-    </div>
+        </a>
+      </div>
     </Link>
   )
 }
