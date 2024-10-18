f1b89791d835151c28e7df3b3e41a8c6a9740e9e
Ida Krech <krech.ida@gmail.com>
2024-10-18 13:01:57 +0200

    Add category menu support in Category page component

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,27 +1,50 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { AppState } from '../redux'
-import ProductGrid from '../components/ProductGrid'
-import { useLocation, useParams } from 'react-router-dom'
-
-
+import { useSelector } from "react-redux"
+import { AppState } from "../redux"
+import ProductGrid from "../components/ProductGrid"
+import { useLocation } from "react-router-dom"
+import useFetchProducts from "../hooks/useFetchProducts"
 
 const CategoryPage = () => {
+  const location = useLocation()
+  const searchParams = new URLSearchParams(location.search)
 
-    const location = useLocation()
-    const searchParams = new URLSearchParams(location.search)
-    
-    // when opened from grid @ Home
-    const gridID = searchParams.get('gridID')
-    const filters = useSelector((state: AppState) => state.filters)
-    const products = useSelector((state: AppState) => state.products.products)
-    const grid = filters.find((grid) => grid.gridID === gridID)
-    const filteredProducts = products.filter((product) => grid?.productIDs.includes(product.id))
+  // when opened from grid @ Home
+  const gridID = searchParams.get("gridID")
+  const filters = useSelector((state: AppState) => state.filters)
+  const products = useSelector((state: AppState) => state.products.products)
+  const grid = filters.find((grid) => grid.gridID === gridID)
+  const filteredProducts = products.filter((product) =>
+    grid?.productIDs.includes(product.id)
+  )
+
+  // when opened from categories menu
+  const type = searchParams.get("type")
+  const category = searchParams.get("category")
+  useFetchProducts([
+    {
+      product_type: type || undefined,
+      product_category: type && category ? category : undefined
+    }
+  ])
+  const typeProducts = useSelector((state: AppState) =>
+    state.products.products.filter((product) => product.product_type === type)
+  )
+  const categoryProducts = typeProducts.filter(
+    (product) => product.category == category
+  )
 
   return (
-    <ProductGrid products={filteredProducts} isExpanded={true} />
+    <ProductGrid
+      products={
+        gridID != null
+          ? filteredProducts
+          : category
+          ? categoryProducts
+          : typeProducts
+      }
+      isExpanded={true}
+    />
   )
 }
-
 export default CategoryPage
