f841235032f7845d71fbf77cbe787ab0a7ef716b
Ida Krech <krech.ida@gmail.com>
2024-10-18 13:37:01 +0200

    Connect Product page to app state to fetch product by ID

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,8 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
+import { IProduct } from '../types/interfaces'
+import { useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { AppState } from '../redux'
+import ProductCard from '../components/ProductCard'
+
 
 const ProductPage = () => {
+  const location = useLocation()
+  const searchParams = new URLSearchParams(location.search)
+  const productID = searchParams.get("id")
+  const parsedID = productID ? parseInt(productID) : undefined
+  const product = useSelector((state: AppState) => state.products.products.find((p) => p.id === parsedID))
   return (
-    <div>ProductPage</div>
+    <div>
+      {product && <ProductCard {...product}/>}
+    </div>
   )
 }
 
