a384ab1aa56d1d6b8ec0506be4ed1b62086b9ea5
Ida Krech <krech.ida@gmail.com>
2024-10-30 20:17:05 +0100

    Add reducer to batch-set multiple products from user database

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { setProductIDs } from "./filterSlice"
 
 interface ICartState {
   products: {
@@ -20,19 +21,46 @@ export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
+    //for products fetched from user data db
+    setProducts(
+      state,
+      action: PayloadAction<
+        {
+          id: number
+          quantity: number
+        }[]
+      >
+    ) {
+      if (state.products.length > 0) {
+        const updatedProducts = state.products.filter(
+          (product) => !action.payload.some((p) => p === product)
+        )
+        state.products = [...updatedProducts, ...action.payload]
+      } else {
+        state.products = action.payload
+      }
+      state.loading = false
+      state.error = null
+    },
     addProduct(state, action: PayloadAction<number>) {
-      const existingProduct = state.products.find((product) => product.id === action.payload)
+      const existingProduct = state.products.find(
+        (product) => product.id === action.payload
+      )
       if (existingProduct) {
         existingProduct.quantity += 1
       } else {
-        state.products.push({id: action.payload, quantity: 1})
+        state.products.push({ id: action.payload, quantity: 1 })
       }
     },
     deleteProduct(state, action: PayloadAction<number>) {
-      state.products = state.products.filter((product) => product.id !== action.payload)
+      state.products = state.products.filter(
+        (product) => product.id !== action.payload
+      )
     },
     decrementProductQuantity(state, action: PayloadAction<number>) {
-      const product = state.products.find((product) => product.id === action.payload)
+      const product = state.products.find(
+        (product) => product.id === action.payload
+      )
       if (product) {
         product.quantity -= 1
         if (product.quantity === 0) {
@@ -45,8 +73,15 @@ export const cartSlice = createSlice({
     },
     setError(state, action: PayloadAction<Error>) {
       state.error = action.payload
-    }
-  }
+      state.loading = false
+    },
+  },
 })
 
-export const { addProduct, deleteProduct, decrementProductQuantity, setError, setLoading } = cartSlice.actions
+export const {
+  addProduct,
+  deleteProduct,
+  decrementProductQuantity,
+  setError,
+  setLoading,
+} = cartSlice.actions
