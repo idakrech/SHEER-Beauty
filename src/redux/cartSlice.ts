bfb976d0ee1c86fe5ec0ba89f08383acf4a495e0
Ida Krech <krech.ida@gmail.com>
2024-10-30 19:12:38 +0100

    Update cart to include product quantity

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,16 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { IProductRefState } from "../interfaces/interfaces"
-import { 
-  setProductIDsHelper, 
-  addProductIDHelper, 
-  deleteProductIDHelper, 
-  setErrorHelper, 
-  setLoadingHelper 
-} from "../helpers/stateHelpers"
 
-const initialState: IProductRefState = {
-  productIDs: [],
+interface ICartState {
+  products: {
+    id: number
+    quantity: number
+  }[]
+  loading: boolean
+  error: Error | null
+}
+
+const initialState: ICartState = {
+  products: [],
   loading: false,
   error: null,
 }
@@ -19,22 +20,33 @@ export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    setProductIDs(state, action: PayloadAction<number[]>) {
-      setProductIDsHelper(state, action)
+    addProduct(state, action: PayloadAction<number>) {
+      const existingProduct = state.products.find((product) => product.id === action.payload)
+      if (existingProduct) {
+        existingProduct.quantity += 1
+      } else {
+        state.products.push({id: action.payload, quantity: 1})
+      }
     },
-    addProductID(state, action: PayloadAction<number>) {
-       addProductIDHelper(state, action)
+    deleteProduct(state, action: PayloadAction<number>) {
+      state.products = state.products.filter((product) => product.id !== action.payload)
     },
-    deleteProductID(state, action: PayloadAction<number>) {
-      deleteProductIDHelper(state, action)
-    },
-    setError(state, action: PayloadAction<Error>) {
-        setErrorHelper(state, action)
+    decrementProductQuantity(state, action: PayloadAction<number>) {
+      const product = state.products.find((product) => product.id === action.payload)
+      if (product) {
+        product.quantity -= 1
+        if (product.quantity === 0) {
+          state.products = state.products.filter((p) => p.id !== action.payload)
+        }
+      }
     },
     setLoading(state, action: PayloadAction<boolean>) {
-        setLoadingHelper(state, action)
+      state.loading = action.payload
+    },
+    setError(state, action: PayloadAction<Error>) {
+      state.error = action.payload
     }
   }
 })
 
-export const { setProductIDs, addProductID, deleteProductID, setError, setLoading } = cartSlice.actions
+export const { addProduct, deleteProduct, decrementProductQuantity, setError, setLoading } = cartSlice.actions
