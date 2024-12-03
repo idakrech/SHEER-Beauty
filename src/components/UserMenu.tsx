e1a47f329440dab13e99ae406608239d461be915
Ida Krech <krech.ida@gmail.com>
2024-12-03 17:23:12 +0100

    Conditionally display user menu or auth form

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,15 +1,30 @@
+import { useSelector } from "react-redux"
+import { AppState } from "../redux"
+import AuthForm from "./AuthForm"
+
 const UserMenu = () => {
 
-const menuItems: string[] = ["Account details", "Order history", "Favorites", "Log out"]
+  const user = useSelector((state: AppState) => state.auth.user)
+
+  const menuItems: string[] = [
+    "Account details",
+    "Order history",
+    "Favorites",
+    "Log out",
+  ]
 
   return (
-   <ul className="p-2">
-    {menuItems.map((item, index) => (
-        <li key={index} className='hover:bg-gray-200'>
-            <p>{item}</p>
+    user ? (
+    <ul className="p-2">
+      {menuItems.map((item, index) => (
+        <li key={index} className="hover:bg-gray-200">
+          <p>{item}</p>
         </li>
-    ))}
-   </ul>
+      ))}
+    </ul>
+    ) : (
+        <AuthForm/>
+    )
   )
 }
 
