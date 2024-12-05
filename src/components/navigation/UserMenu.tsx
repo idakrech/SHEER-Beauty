06c15f7908836397b6c48c5268ae2bdd2b002da8
Ida Krech <krech.ida@gmail.com>
2024-12-05 20:52:45 +0100

    Add routing from user navigation menu to user page

--- src/components/navigation/UserMenu.tsx
+++ src/components/navigation/UserMenu.tsx
@@ -1,29 +1,32 @@
 import { useSelector } from "react-redux"
 import { AppState } from "../../redux"
 import AuthForm from "../user-page/AuthForm"
+import { Link } from "react-router-dom"
 
 const UserMenu = () => {
 
   const user = useSelector((state: AppState) => state.auth.user)
 
-  const menuItems: string[] = [
-    "Account details",
-    "Order history",
-    "Favorites",
-    "Log out",
+  const menuItems = [
+    {label: "Account details", tab: "account-details"},
+    {label: "Order history", tab: "order-history"},
+    {label: "Favorites", tab: "favorites"},
+    {label: "Log out", tab: "log-out"}
   ]
 
   return (
     user ? (
-    <ul className="p-2">
-      {menuItems.map((item, index) => (
-        <li key={index} className="hover:bg-gray-200">
-          <p>{item}</p>
-        </li>
-      ))}
-    </ul>
+      <ul className="p-2">
+        {menuItems.map((item, index) => (
+          <li key={index} className="hover:bg-gray-200">
+            <Link to={`/user-page/${item.tab}`}>
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
     ) : (
-        <AuthForm/>
+      <AuthForm/>
     )
   )
 }
--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import AddressForm from '../components/AddressForm'
+import { useParams } from 'react-router-dom'
+import UserMenu from '../components/navigation/UserMenu'
+import OrderHistory from '../components/user-page/OrderHistory'
+import { useSelector } from 'react-redux'
+import { AppState } from '../redux'
+import AddressForm from '../components/user-page/AddressForm'
+import AuthForm from '../components/user-page/AuthForm'
 
 const UserPage = () => {
+
+  const { tab } = useParams<{ tab: string }>()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const favorites = useSelector((state: AppState) => state.favorites.productIDs)
+
   return (
-    <AddressForm/>
+    <div>
+    <UserMenu />
+    {tab === "account-details" && <AddressForm />}
+    {tab === "order-history" && <OrderHistory />}
+    {tab === "log-out" && <AuthForm />}
+    {/* {tab === "favorites" && <ProductGrid products={favorites} isExpanded={false}/>} */}
+    </div>
   )
 }
 
