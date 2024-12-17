b92e78865b5f8abcef1d100c5c749fe1f943cb43
Ida Krech <krech.ida@gmail.com>
2024-12-17 19:21:58 +0100

    Add new main font - Outfit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F7F7F7"
+      }
+    },
+    fontFamily: {
+      outfit: ["Outfit", 'sans-serif']
+    }
   },
   plugins: [],
 }
