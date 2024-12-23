ba8142e5d4f5766a434b4959a8c9f88856a453cd
Ida Krech <krech.ida@gmail.com>
2024-12-23 16:39:13 +0100

    Fetch and render Home page products directly from API to avoid long loading time

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,62 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import useFetchProducts from "../hooks/useFetchProducts"
-import { useDispatch, useSelector } from "react-redux"
-import { AppDispatch, AppState } from "../redux"
-import { filterProducts } from "../utils/filterProducts"
-import { useEffect } from "react"
-import { resetProducts } from "../redux/productsSlice"
+import { useSelector } from "react-redux"
+import ProductGrid from "../components/product-display/ProductGrid"
+import { useEffect, useState } from "react"
+import { IProduct } from "../interfaces/interfaces"
+import APIService from "../services/APIService"
+import { AppState } from "../redux"
 
 const Home = () => {
   const state = useSelector((state: AppState) => state)
-  const filterParams = state.filters.map((f) => f.fetchParams)
+  const [productsByFilter, setProductsByFilter] = useState<{
+    [key: string]: IProduct[]
+  }>({}) // example:
+  // {
+  //   grid1: [prod1, prod2, prod3],
+  //   grid2: [prod4, prod5],
+  //   grid6: [prod6]
+  // }
 
-  useFetchProducts(filterParams)
-  
+  // Here I fetch and render products directly from API and there is no use of app state here.
+  // This is due to long loading time when saving all api products in app state on app launch.
+  // That long process of uploading api products into state runs in the background while products in Home page appear right away.
+  useEffect(() => {
+    state.filters.forEach(async (filter) => {
+      try {
+        const products = await APIService.fetchProducts(filter.fetchParams)
+
+        setProductsByFilter((prevState) => ({
+          ...prevState,
+          [filter.gridID]: products,
+        }))
+      } catch (error) {
+        console.error(
+          "Error fetching products for filter:",
+          filter.gridID,
+          error
+        )
+      }
+    })
+  }, [state.filters])
 
   return (
     <>
-      {filterProducts(state)}
+      {Object.keys(productsByFilter).map((gridID) => {
+        const filter = state.filters.find((f) => f.gridID === gridID)
+
+        if (!filter) return null
+
+        return (
+          <div key={gridID}>
+            <h1>{filter.title}</h1>
+            <ProductGrid
+              products={productsByFilter[gridID]}
+              isExpanded={false}
+              category={gridID}
+              maxLimit={9}
+            />
+          </div>
+        )
+      })}
     </>
   )
 }
--- src/redux/filterSlice.ts
+++ src/redux/filterSlice.ts
@@ -1,12 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { IFetchProductParams } from "../interfaces/interfaces";
 
 export interface IGridFilter {
   gridID: string;
   title: string;
   fetchParams: IFetchProductParams;
-  productIDs: number[];
   createdAt?: string; //necessary for new arrivals section
 }
 
@@ -17,38 +15,22 @@ export const initialState: IFilterState = [
     gridID: "grid1",
     title: "Vegan products",
     fetchParams: { product_type: "eyeshadow", product_tags: ["Vegan"] },
-    productIDs: [],
   },
   {
     gridID: "grid2",
     title: "Maybelline mascaras",
     fetchParams: { product_type: "Mascara", brand: "Maybelline" },
     createdAt: "5/2/2014",
-    productIDs: [],
   },
   {
     gridID: "grid3",
     title: "Highest rated Nyx products",
     fetchParams: { rating_greater_than: 4, brand: "nyx" },
-    productIDs: [],
   },
 ];
 
 export const filterSlice = createSlice({
   name: "filters",
   initialState,
-  reducers: {
-    setProductIDs(
-      state,
-      action: PayloadAction<{ gridID: string; productIDs: number[] }>
-    ) {
-      const { gridID, productIDs } = action.payload;
-      const grid = state.find((g) => g.gridID === gridID);
-      if (grid) {
-        grid.productIDs = productIDs;
-      }
-    }
-  }
+  reducers: {}
 })
-
-export const { setProductIDs } = filterSlice.actions
