37f03c807cb0dcb44e06095bbc89ed9e953ca848
Ida Krech <krech.ida@gmail.com>
2025-01-14 19:48:32 +0100

    Extend logout logic to clean app state on logout

--- src/components/navigation/UserMenu.tsx
+++ src/components/navigation/UserMenu.tsx
@@ -1,18 +1,33 @@
-import { useSelector } from "react-redux"
-import { AppState } from "../../redux"
+import { useDispatch, useSelector } from "react-redux"
+import { AppDispatch, AppState } from "../../redux"
 import AuthForm from "../user-page/AuthForm"
 import { Link } from "react-router-dom"
+import { setProducts as setCartProducts } from "../../redux/cartSlice"
+import { setProducts as setFavProducts} from "../../redux/favoritesSlice"
 import { logOut } from "../../services/authService"
 
 const UserMenu = () => {
 
   const user = useSelector((state: AppState) => state.auth.user)
 
+  const dispatch = useDispatch<AppDispatch>()
+
   const menuItems = [
     {label: "Account details", tab: "account-details"},
     {label: "Order history", tab: "order-history"},
     {label: "Favorites", tab: "favorites"},
   ]
+
+  const handleLogout = async () => {
+      try {
+        await logOut()
+        dispatch(setFavProducts([]))
+        dispatch(setCartProducts([]))
+        console.log("User logged out")
+      } catch (error) {
+        alert(`An error occured: ${error}`)
+      }
+    }
 
   return (
     user ? (
@@ -24,7 +39,7 @@ const UserMenu = () => {
             </Link>
           </li>
         ))}
-        <li><button  onClick={() => logOut()}>Log out</button></li>
+        <li><button  onClick={() => handleLogout()}>Log out</button></li>
       </ul>
     ) : (
       <AuthForm/>
