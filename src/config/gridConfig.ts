aa61006450baab623fa759f03c15382ccfdfc75f
Ida Krech <krech.ida@gmail.com>
2025-01-16 17:55:44 +0100

    Move dynamic home page grids' filters from redux state to a config file

---
+++ src/config/gridConfig.ts
@@ -0,0 +1,18 @@
+export const gridConfig = [
+    {
+      gridID: "grid1",
+      title: "Vegan products",
+      filters: { product_type: "eyeshadow", tag_list: ["Vegan"] },
+    },
+    {
+      gridID: "grid2",
+      title: "Maybelline mascaras",
+      filters: { product_type: "mascara", brand: "maybelline" },
+    },
+    {
+      gridID: "grid3",
+      title: "Highest rated Nyx products",
+      filters: { rating_greater_than: 4, brand: "nyx" },
+    },
+  ];
+  
