61bbcd5622d7f07227085e2b06e4a095e5be2ea0
Ida Krech <krech.ida@gmail.com>
2025-01-21 16:50:45 +0100

    Add filters & title props. Pass these props as URL parameters when navigating to promoted products page. Remove category prop.

--- src/components/product-display/ProductGrid.tsx
+++ src/components/product-display/ProductGrid.tsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from "react"
 import ProductCard from "./ProductCard"
 import { IProduct } from "../../interfaces/interfaces"
-import { useNavigate } from "react-router-dom";
-import checkIfImageExists from "../../helpers/checkImage";
+import { useNavigate } from "react-router-dom"
+import checkIfImageExists from "../../helpers/checkImage"
+import { IFilterState } from "../../redux/filterSlice"
 
 interface IProductGridProps {
-  products: IProduct[],
-  isExpanded: boolean,
-  category?: string,
+  products: IProduct[]
+  isExpanded: boolean
   maxLimit?: number
+  filters?: IFilterState
+  title: string
 }
 
-const ProductGrid: React.FC<IProductGridProps> = ({ products, isExpanded, category, maxLimit }) => {
+const ProductGrid: React.FC<IProductGridProps> = ({
+  products,
+  isExpanded,
+  maxLimit,
+  filters,
+  title,
+}) => {
   const navigate = useNavigate()
   const [filteredProducts, setFilteredProducts] = useState<IProduct[]>([])
 
@@ -27,25 +35,48 @@ const ProductGrid: React.FC<IProductGridProps> = ({ products, isExpanded, catego
             })
         )
       )
-  
-      setFilteredProducts(productsWithImages.filter((product) => product !== null) as IProduct[])
+      //TODO: filter out products with no brand
+      setFilteredProducts(
+        productsWithImages.filter((product) => product !== null) as IProduct[]
+      )
     }
-  
     filterProductsWithImages()
   }, [products])
-  
 
   return (
-    <div className="grid grid-cols-5 gap-4 justify-items-center justify-center py-5">
-      {filteredProducts
-        .slice(0, maxLimit ?? filteredProducts.length)
-        .map((product) => (
-          <ProductCard key={product.id} {...product} />
-        ))}
+    <div>
+      <h3>{title}</h3>
+      <div className="grid grid-cols-5 gap-4 justify-items-center justify-center py-5">
+        {filteredProducts
+          .slice(0, maxLimit ?? filteredProducts.length)
+          .map((product) => (
+            <ProductCard key={product.id} {...product} />
+          ))}
 
-      {!isExpanded && (
-        <button onClick={() => navigate(`/category-page?gridID=${category}`)}>More</button>
-      )}
+        {!isExpanded && (
+          <button
+            onClick={() => {
+              const params = new URLSearchParams()
+
+              filters?.selectedBrands.forEach((brand) =>
+                params.append("brand", brand.toLowerCase())
+              )
+              filters?.selectedTags.forEach((tag) => params.append("tag", tag.toLowerCase()))
+              if (filters?.priceRange?.min)
+                params.append("price_min", filters.priceRange.min.toString())
+              if (filters?.priceRange?.max)
+                params.append("price_max", filters.priceRange.max.toString())
+              
+              if (title) {
+                params.append("title", title)
+              }
+              navigate(`/promoted?${params.toString()}`)
+            }}
+          >
+            More
+          </button>
+        )}
+      </div>
     </div>
   )
 }
