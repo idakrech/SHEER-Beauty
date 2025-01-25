78e7249fa723f65e7d37b0f07a3355a66d49d21b
Ida Krech <krech.ida@gmail.com>
2025-01-25 21:45:28 +0100

    Add address state. Pass address state setting as callback arg in Address Form

--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react"
 import { useUserData } from "../../hooks/useUserData"
 import AddressForm from "../user-page/AddressForm"
+import Delivery from "./Delivery"
+import Payment from "./Payment"
+import { IAddress } from "../../interfaces/interfaces"
 
 const Checkout = () => {
   const { userDataFromDb, loading } = useUserData()
+  const [address, setAddress] = useState<IAddress>()
+
+  useEffect(() => {
+    if (userDataFromDb?.address) {
+      setAddress(userDataFromDb?.address)
+    }
+  }, [userDataFromDb])
 
   if (loading) {
     return <div>Loading...</div>
@@ -11,7 +22,16 @@ const Checkout = () => {
   return (
     <div>
       <h3>Checkout</h3>
-      {userDataFromDb?.address ? <AddressForm address={userDataFromDb?.address}/> : <AddressForm/>}
+      {userDataFromDb?.address ? (
+        <AddressForm
+          address={userDataFromDb?.address}
+          onAddressChange={setAddress}
+        />
+      ) : (
+        <AddressForm onAddressChange={setAddress} />
+      )}
+      {address && <Delivery address={address} />}
+      <Payment />
       {/* TODO: delivery form */}
       {/* TODO: payment form */}
     </div>
