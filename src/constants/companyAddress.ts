7ac160001643a8ca6466095b7b2e23dc50806cfb
Ida Krech <krech.ida@gmail.com>
2025-01-25 21:39:27 +0100

    Create constant with company address

---
+++ src/constants/companyAddress.ts
@@ -0,0 +1,13 @@
+import { AddressCreateRequest } from "shippo";
+
+
+export const companyAddress: AddressCreateRequest = {
+    company: "Beauty Webshop",
+    street1: "1320 Kuhl Avenue",
+    city: "Atlanta",
+    state: "Georgia",
+    zip: "30303",
+    country: "US",
+    phone: "+1 6786801047",
+    email: "bueaty@beautywebshop.com"
+}
