915d88d40c147c3b82081f3db1247cb33f5f7953
Ida Krech <krech.ida@gmail.com>
2025-01-26 14:02:51 +0100

    Make callback argument optional

--- src/components/user-page/AddressForm.tsx
+++ src/components/user-page/AddressForm.tsx
@@ -5,7 +5,13 @@ import { IAddress } from "../../interfaces/interfaces"
 import { userDataService } from "../../services/userDataService"
 
 //TODO: replace my own address interface with Shippo interface
-const AddressForm = ({address, onAddressChange}: {address?: IAddress, onAddressChange: (address: IAddress) => void}) => {
+const AddressForm = ({
+  address,
+  onAddressChange,
+}: {
+  address?: IAddress
+  onAddressChange?: (address: IAddress) => void
+}) => {
   const user = useSelector((state: AppState) => state.auth.user)
   const [addressValues, setAddressValues] = useState<IAddress>({
     firstName: "",
@@ -26,7 +32,9 @@ const AddressForm = ({address, onAddressChange}: {address?: IAddress, onAddressC
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setAddressValues({ ...addressValues, [e.target.name]: e.target.value })
-    onAddressChange({ ...addressValues, [e.target.name]: e.target.value })
+    if (onAddressChange) {
+      onAddressChange({ ...addressValues, [e.target.name]: e.target.value })
+    }
   }
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -43,71 +51,69 @@ const AddressForm = ({address, onAddressChange}: {address?: IAddress, onAddressC
   }
 
   return (
-    <div>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <input
-              name="firstName"
-              type="text"
-              placeholder="First Name"
-              value={addressValues.firstName}
-              onChange={changeHandler}
-            />
-            <input
-              name="lastName"
-              type="text"
-              placeholder="Last Name"
-              value={addressValues.lastName}
-              onChange={changeHandler}
-            />
-            <input
-              name="street"
-              type="text"
-              placeholder="Street"
-              value={addressValues.street}
-              onChange={changeHandler}
-            />
-            <input
-              name="city"
-              type="text"
-              placeholder="City"
-              value={addressValues.city}
-              onChange={changeHandler}
-            />
-            <input
-              name="zipCode"
-              type="text"
-              placeholder="Zip Code"
-              value={addressValues.zipCode}
-              onChange={changeHandler}
-            />
-            {/* TODO: country dropdown */}
-            <input
-              name="country"
-              type="text"
-              placeholder="Country"
-              value={addressValues.country}
-              onChange={changeHandler}
-            />
-            {/* TODO: dropdown of country codes; generate placeholder based on country input above */}
-            <input
-              name="phoneCountryCode"
-              type="text"
-              placeholder="Country code"
-              value={addressValues.phoneCountryCode}
-              onChange={changeHandler}
-            />
-            <input
-              name="phoneNumber"
-              type="number"
-              placeholder="Phone number"
-              value={addressValues.phoneNumber ? addressValues.phoneNumber : ""}
-              onChange={changeHandler}
-            />
-            {/* TODO: if it's checkout, then don't show save btn? */}
-            <button type="submit">Save</button>
-          </form>
-        </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="firstName"
+            type="text"
+            placeholder="First Name"
+            value={addressValues.firstName}
+            onChange={changeHandler}
+          />
+          <input
+            name="lastName"
+            type="text"
+            placeholder="Last Name"
+            value={addressValues.lastName}
+            onChange={changeHandler}
+          />
+          <input
+            name="street"
+            type="text"
+            placeholder="Street"
+            value={addressValues.street}
+            onChange={changeHandler}
+          />
+          <input
+            name="city"
+            type="text"
+            placeholder="City"
+            value={addressValues.city}
+            onChange={changeHandler}
+          />
+          <input
+            name="zipCode"
+            type="text"
+            placeholder="Zip Code"
+            value={addressValues.zipCode}
+            onChange={changeHandler}
+          />
+          {/* TODO: country dropdown */}
+          <input
+            name="country"
+            type="text"
+            placeholder="Country"
+            value={addressValues.country}
+            onChange={changeHandler}
+          />
+          {/* TODO: dropdown of country codes; generate placeholder based on country input above */}
+          <input
+            name="phoneCountryCode"
+            type="text"
+            placeholder="Country code"
+            value={addressValues.phoneCountryCode}
+            onChange={changeHandler}
+          />
+          <input
+            name="phoneNumber"
+            type="number"
+            placeholder="Phone number"
+            value={addressValues.phoneNumber ? addressValues.phoneNumber : ""}
+            onChange={changeHandler}
+          />
+          {/* TODO: if it's checkout, then don't show save btn? */}
+        <button type="submit">Save</button>
+      </form>
     </div>
   )
 }
