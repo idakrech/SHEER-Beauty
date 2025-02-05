0a9b3a2bc94e362d1bbae17c7d4fb3a7ce60e2de
Ida Krech <krech.ida@gmail.com>
2025-02-05 16:45:05 +0100

    Check if address is complete before fetching shipment options. Disable shipment options while editing the address.

--- src/components/checkout/Delivery.tsx
+++ src/components/checkout/Delivery.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { getShipment } from "../../services/shipmentService"
 import { IAddress } from "../../interfaces/interfaces"
 import { AddressValidationResultsMessage, Rate } from "shippo"
 import { useUserData } from "../../hooks/useUserData"
 import AddressForm from "../user-page/AddressForm"
-import { getFriendlyMessage } from "../../helpers/formatValidationMessage"
+import { getSummaryMessage } from "../../helpers/formatValidationMessage"
 
 const Delivery = () => {
   const { userDataFromDb, loading } = useUserData()
@@ -15,12 +15,33 @@ const Delivery = () => {
   const [addressValidationMessages, setAddressValidationMessages] = useState<
     AddressValidationResultsMessage[] | undefined
   >([])
+  const isAddressComplete = useRef<boolean>(false)
+  const [isEditing, setIsEditing] = useState<boolean>(false)
 
   useEffect(() => {
     if (userDataFromDb?.address) {
       setAddress(userDataFromDb?.address)
+      isAddressComplete.current = true
     }
   }, [userDataFromDb?.address])
+
+  const checkIfAddressComplete = (address: IAddress) => {
+    const isComplete =
+      !!address.name &&
+      !!address.street1 &&
+      !!address.city &&
+      !!address.zip &&
+      !!address.state &&
+      !!address.country &&
+      !!address.phone
+    isAddressComplete.current = isComplete
+  }
+
+  useEffect(() => {
+    if (address) {
+      checkIfAddressComplete(address)
+    }
+  }, [address])
 
   const fetchRates = async () => {
     setShipmentLoading(true)
@@ -30,7 +51,7 @@ const Delivery = () => {
         const shipment = await getShipment(address)
         setRates(shipment.rates)
         setAddressValidationMessages(
-          shipment.addressTo.validationResults?.messages
+          shipment.addressTo?.validationResults?.messages
         )
         setAddress(shipment.addressTo)
       }
@@ -39,6 +60,7 @@ const Delivery = () => {
       setError(
         "An error occured. Please make sure to provide all the necessary address information"
       )
+      setAddressValidationMessages([])
     } finally {
       setShipmentLoading(false)
     }
@@ -52,35 +74,40 @@ const Delivery = () => {
         <>
           <div>
             <h3>Checkout</h3>
-            <AddressForm address={address} onAddressChange={setAddress} />
+            <AddressForm
+              address={address}
+              onAddressChange={(updatedAddress, editing) => {
+                setAddress(updatedAddress)
+                checkIfAddressComplete(updatedAddress)
+                setIsEditing(editing)
+              }}
+              onAddressCompletion={(isComplete) => {
+                isAddressComplete.current = isComplete
+              }}
+            />
           </div>
           {addressValidationMessages &&
             addressValidationMessages.length > 0 && (
-              <div>
-                <h4>Address Suggestions:</h4>
-                <ul>
-                  {addressValidationMessages.map((message, index) => (
-                    <li key={index}>
-                      {message.text && getFriendlyMessage(message.text)}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+              <div>{getSummaryMessage(addressValidationMessages)}</div>
             )}
           <h1>Shipping Options</h1>
-          <button onClick={fetchRates}>
-            {shipmentLoading ? "Loading..." : "Get Shipping Rates"}
-          </button>
-          {shipmentLoading && <p>Fetching shipment rates...</p>}
-          {error && <p style={{ color: "red" }}>{error}</p>}
-          <ul>
-            {rates.map((rate) => (
-              <li key={rate.servicelevel.token}>
-                {rate.servicelevel.name} - {rate.amount} {rate.currency}{" "}
-                {`(${rate.amountLocal} ${rate.currencyLocal})`}
-              </li>
-            ))}
-          </ul>
+          <div className={`${isEditing ? "opacity-50 pointer-events-none" : "opacity-100 pointer-events-auto"}`}>
+            {/* TODO: make save btn also fetch rates btn */}
+            <button onClick={fetchRates} disabled={isEditing}> 
+              {/* !isAddressComplete.current */}
+              {shipmentLoading ? "Loading..." : "Get Shipping Rates"}
+            </button>
+            {shipmentLoading && <p>Fetching shipment rates...</p>}
+            {error && <p style={{ color: "red" }}>{error}</p>}
+            <ul>
+              {rates.map((rate) => (
+                <li key={rate.servicelevel.token}>
+                  {rate.servicelevel.name} - {rate.amount} {rate.currency}{" "}
+                  {`(${rate.amountLocal} ${rate.currencyLocal})`}
+                </li>
+              ))}
+            </ul>
+          </div>
         </>
       )}
     </div>
--- src/components/user-page/AddressForm.tsx
+++ src/components/user-page/AddressForm.tsx
@@ -7,9 +7,11 @@ import { userDataService } from "../../services/userDataService"
 const AddressForm = ({
   address,
   onAddressChange,
+  onAddressCompletion,
 }: {
   address?: IAddress
-  onAddressChange?: (address: IAddress) => void
+  onAddressChange?: (address: IAddress, isEditing: boolean) => void
+  onAddressCompletion?: (isComplete: boolean) => void
 }) => {
   const user = useSelector((state: AppState) => state.auth.user)
   const [addressValues, setAddressValues] = useState<IAddress>({
@@ -29,9 +31,22 @@ const AddressForm = ({
   }, [address])
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setAddressValues({ ...addressValues, [e.target.name]: e.target.value })
+    const updatedAddress = { ...addressValues, [e.target.name]: e.target.value }
+    setAddressValues(updatedAddress)
     if (onAddressChange) {
-      onAddressChange({ ...addressValues, [e.target.name]: e.target.value })
+      onAddressChange(updatedAddress, true)
+    }
+
+    const isComplete =
+      !!updatedAddress.name &&
+      !!updatedAddress.street1 &&
+      !!updatedAddress.city &&
+      !!updatedAddress.zip &&
+      !!updatedAddress.country &&
+      !!updatedAddress.phone
+    
+    if (onAddressCompletion) {
+      onAddressCompletion(isComplete)
     }
   }
 
@@ -47,63 +62,86 @@ const AddressForm = ({
       //TODO: useState error instead of alert
       alert("An error occured")
     }
+    if (onAddressCompletion) {
+      if (
+        !!addressValues.name ||
+        !!addressValues.street1 ||
+        !!addressValues.city ||
+        !!addressValues.zip ||
+        !!addressValues.country ||
+        !!addressValues.phone
+      ) {
+        onAddressCompletion(false)
+      } else {
+        onAddressCompletion(true)
+      }
+    }
+    if (onAddressChange) {
+      onAddressChange(addressValues, false)
+    }
   }
 
   return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            name="name"
-            type="text"
-            placeholder="Name"
-            value={addressValues.name}
-            onChange={changeHandler}
-          />
-          <input
-            name="street1"
-            type="text"
-            placeholder="Street"
-            value={addressValues.street1}
-            onChange={changeHandler}
-          />
-          <input
-            name="city"
-            type="text"
-            placeholder="City"
-            value={addressValues.city}
-            onChange={changeHandler}
-          />
-          <input
-            name="zip"
-            type="text"
-            placeholder="Zip Code"
-            value={addressValues.zip}
-            onChange={changeHandler}
-          />
-          <input
-            name="state"
-            type="text"
-            placeholder="State/Provice"
-            value={addressValues.state}
-            onChange={changeHandler}
-          />
-          {/* TODO: country dropdown */}
-          <input
-            name="country"
-            type="text"
-            placeholder="Country"
-            value={addressValues.country}
-            onChange={changeHandler}
-          />
-          {/* TODO: dropdown of country codes; generate placeholder based on country input above */}
-          <input
-            name="phone"
-            type="number"
-            placeholder="Phone number"
-            value={addressValues.phone}
-            onChange={changeHandler}
-          />
-          {/* TODO: if it's checkout, then don't show save btn? */}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          value={addressValues.name}
+          onChange={changeHandler}
+          required
+        />
+        <input
+          name="street1"
+          type="text"
+          placeholder="Street"
+          value={addressValues.street1}
+          onChange={changeHandler}
+          required
+        />
+        <input
+          name="city"
+          type="text"
+          placeholder="City"
+          value={addressValues.city}
+          onChange={changeHandler}
+          required
+        />
+        <input
+          name="zip"
+          type="text"
+          placeholder="Zip Code"
+          value={addressValues.zip}
+          onChange={changeHandler}
+          required
+        />
+        <input
+          name="state"
+          type="text"
+          placeholder="State/Provice"
+          value={addressValues.state}
+          onChange={changeHandler}
+        />
+        {/* TODO: country dropdown */}
+        <input
+          name="country"
+          type="text"
+          placeholder="Country"
+          value={addressValues.country}
+          onChange={changeHandler}
+          required
+        />
+        {/* TODO: dropdown of country codes; generate placeholder based on country input above */}
+        <input
+          name="phone"
+          type="number"
+          placeholder="Phone number"
+          value={addressValues.phone}
+          onChange={changeHandler}
+          required
+        />
+        {/* TODO: if it's checkout, then don't show save btn? */}
         <button type="submit">Save</button>
       </form>
     </div>
