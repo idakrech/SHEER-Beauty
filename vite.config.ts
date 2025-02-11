cfe94d5fd7e1dde3beea8484b124844da34df0d1
Ida Krech <krech.ida@gmail.com>
2025-02-11 17:16:26 +0100

    Configure HTTPS for Vite server and ignore SSL certs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+
+const sslCertPath = process.env.SSL_CERT_PATH
+const sslKeyPath = process.env.SSL_KEY_PATH
+
+if (!sslCertPath || !sslKeyPath) {
+  throw new Error("SSL certificate and key paths must be defined in .env")
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(sslKeyPath),
+      cert: fs.readFileSync(sslCertPath),
+    }
+  }
 })
