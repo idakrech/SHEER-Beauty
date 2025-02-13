69f40fd530382e35ec87f5a52b1c3470a8eb8ea2
Ida Krech <krech.ida@gmail.com>
2025-02-13 17:35:34 +0100

    Create transaction interface

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,45 +1,46 @@
-import { Address } from "shippo";
+import { Timestamp } from "firebase/firestore"
+import { Address } from "shippo"
 
 export interface IFetchProductParams {
-  brand?: string;
-  product_type?: string;
-  product_category?: string;
-  product_tags?: string[];
-  price_greater_than?: number;
-  price_less_than?: number;
-  rating_greater_than?: number;
-  rating_less_than?: number;
+  brand?: string
+  product_type?: string
+  product_category?: string
+  product_tags?: string[]
+  price_greater_than?: number
+  price_less_than?: number
+  rating_greater_than?: number
+  rating_less_than?: number
 }
 
 export interface IProductColor {
-  hex_value: string;
-  colour_name: string;
+  hex_value: string
+  colour_name: string
 }
 
 export interface IProduct {
-  id: number;
-  brand: string;
-  name: string;
-  price: string;
-  price_sign: string | null;
-  currency: string | null;
-  image_link: string;
-  product_link: string;
-  website_link: string;
-  description: string;
-  rating: number | null;
-  category: string | null;
-  product_type: string;
-  tag_list: string[];
-  created_at: string;
-  updated_at: string;
-  product_api_url: string;
-  api_featured_image: string;
-  product_colors: IProductColor[];
+  id: number
+  brand: string
+  name: string
+  price: string
+  price_sign: string | null
+  currency: string | null
+  image_link: string
+  product_link: string
+  website_link: string
+  description: string
+  rating: number | null
+  category: string | null
+  product_type: string
+  tag_list: string[]
+  created_at: string
+  updated_at: string
+  product_api_url: string
+  api_featured_image: string
+  product_colors: IProductColor[]
 }
 
 export interface IProductType {
-  name: string,
+  name: string
   categories: string[] | null
 }
 
@@ -51,8 +52,8 @@ export interface IUserData {
   address: IAddress | null
   favorites?: number[]
   cart?: {
-      productId: number
-      quantity: number
+    productId: number
+    quantity: number
   }[]
   transactionIDs?: string[]
 }
@@ -61,4 +62,11 @@ export interface IUser {
   uid: string
   email: string | null
   displayName: string | null
+}
+
+export interface ITransaction {
+  cart: { product: IProduct; quantity: number }[]
+  createdAt: Timestamp
+  shipmentAddress: IAddress
+  paymentMethod: "card" | "applePay/googlePay"
 }
