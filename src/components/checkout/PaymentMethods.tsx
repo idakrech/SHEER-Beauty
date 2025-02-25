89fa88dfcc424c6ddc7244811ac33efe11d93bc6
Ida Krech <krech.ida@gmail.com>
2025-02-25 15:47:27 +0100

    Replace select with radio buttons. Style component

--- src/components/checkout/PaymentMethods.tsx
+++ src/components/checkout/PaymentMethods.tsx
@@ -3,22 +3,65 @@ import CardPayment from "./CardPayment"
 import GoogleApplePay from "./GoogleApplePay"
 import { IAddress } from "../../interfaces/interfaces"
 
-const PaymentMethods = ({totalSum, shipmentAddress} : {totalSum: number, shipmentAddress: IAddress}) => {
-    const [selectedMethod, setSelectedMethod] = useState<string>("card")
-  
-    return (
-      <div>
-        <h2>Select Payment Method</h2>
-        <select onChange={(e) => setSelectedMethod(e.target.value)} value={selectedMethod}>
-          <option value="card">Credit / Debit Card</option>
-          <option value="google">Google Pay / Apple Pay</option>
-        </select>
-  
-        {selectedMethod === "card" && <CardPayment shipmentAddress={shipmentAddress}/>}
+const PaymentMethods = ({
+  totalSum,
+  shipmentAddress
+}: {
+  totalSum: number
+  shipmentAddress: IAddress
+}) => {
+  const [selectedMethod, setSelectedMethod] = useState<string>("card")
+
+  return (
+    <div className="bg-white p-4 w-full border border-zinc-300 mb-4 text-zinc-700">
+      <h3 className="text-xl text-zinc-700 font-serif font-bold mx-3 mt-2 border-b border-zinc-300 pb-1">
+        Payment
+      </h3>
+
+      <div className="p-3">
+        <h2 className="font-bold">Select Payment Method</h2>
+        <div className="my-2">
+          <label className="flex items-center">
+            <input
+              type="radio"
+              name="payment"
+              value="card"
+              checked={selectedMethod === "card"}
+              onChange={(e) => setSelectedMethod(e.target.value)}
+              className="accent-accent"
+            />
+            <span className="ml-2">Credit/Debit Card</span>
+          </label>
+
+          <label className="flex items-center">
+            <input
+              type="radio"
+              name="payment"
+              value="google"
+              checked={selectedMethod === "google"}
+              onChange={(e) => setSelectedMethod(e.target.value)}
+              className="accent-accent"
+            />
+            <span className="ml-2">Google Pay/Apple Pay</span>
+          </label>
+        </div>
+
+        {selectedMethod === "card" && (
+          <div className="w-full mt-3">
+            <CardPayment
+              shipmentAddress={shipmentAddress}
+            />
+          </div>
+        )}
         {/* TODO: add disclaimer and warning not to perform real payment through the provider */}
-        {selectedMethod === "google" && <GoogleApplePay sum={totalSum}/>}
+        {selectedMethod === "google" && (
+          <div className="my-2 w-1/2 mt-3">
+            <GoogleApplePay sum={totalSum} />
+          </div>
+        )}
       </div>
-    )
-  }
-  
-  export default PaymentMethods
+    </div>
+  )
+}
+
+export default PaymentMethods
