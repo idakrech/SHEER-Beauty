f488cffd2d3521933361f0874beab37c8fb05701
Ida Krech <krech.ida@gmail.com>
2025-02-26 20:53:33 +0100

    Fetch total sum from state instead of calculating it

--- src/hooks/useTransaction.ts
+++ src/hooks/useTransaction.ts
@@ -4,16 +4,14 @@ import { ITransaction } from "../interfaces/interfaces"
 import { Timestamp } from "firebase/firestore"
 import { userDataService } from "../services/userDataService"
 import { setProducts } from "../redux/cartSlice"
-import { useShoppingCart } from "./useShoppingCart"
 import { setAddress } from "../redux/transactionDraftSlice"
 
 export function useTransaction() {
   const cartProducts = useSelector((state: AppState) => state.cart.products)
   const user = useSelector((state: AppState) => state.auth.user)
   const paymentMethod = useSelector((state: AppState) => state.transactionDraft.payment.method)
-  const { priceSum } = useShoppingCart()
   const delivery = useSelector((state: AppState) => state.transactionDraft.delivery)
-  const totalSum = delivery.rate ? parseFloat(delivery.rate.toFixed(2)) + parseFloat(priceSum.toFixed(2)) : parseFloat(priceSum.toFixed(2))
+  const totalSum = useSelector((state: AppState) => state.transactionDraft.payment.totalSum)
   const dispatch = useDispatch<AppDispatch>()
 
   const finalAddress = delivery.correctedAddress ?? delivery.address ?? {
