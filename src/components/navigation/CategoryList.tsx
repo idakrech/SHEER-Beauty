c511e9141bab8d8ff0ade57bebe4200aa61f67a9
Ida Krech <krech.ida@gmail.com>
2025-02-27 23:06:47 +0100

    Add hover to list elements, change background to white

--- src/components/navigation/CategoryList.tsx
+++ src/components/navigation/CategoryList.tsx
@@ -6,18 +6,17 @@ import { formatParamName } from "../../helpers/formatParamNames"
 const CategoryList = () => {
   const types = productTypes
 
-  // STYLE: wrap categories into columns instead of one long list
   return (
-    <ul className={`p-5 text-left font-sans font-light grid gap-5 grid-cols-10`}>
+    <ul className="p-5 text-left font-sans font-light grid gap-5 grid-cols-10">
     {types.map((type, index) => (
       <li key={index} className="mb-2">
         <Link to={`/category-page?type=${type.name}`}>
           <p className="font-sans font-semibold mb-1 underline">{formatParamName(type.name)}</p>
         </Link>
         <ul className="text-left">
-          <li><Link to={`/category-page?type=${type.name}`}>All</Link></li>
+          <li className="hover:text-dark"><Link to={`/category-page?type=${type.name}`}>All</Link></li>
           {type.categories?.map((cat, i) => (
-            <li key={i}>
+            <li key={i} className="hover:text-dark">
               <Link to={`/category-page?type=${type.name}&category=${cat}`}>
                 {formatParamName(cat)}
               </Link>
