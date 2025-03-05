29e959c47307c9120773af5188229309e133bcd2
Ida Krech <krech.ida@gmail.com>
2025-03-05 20:59:16 +0100

    Add Embla lib for horizontal scroll of product colors

--- src/components/product-display/ProductCard.tsx
+++ src/components/product-display/ProductCard.tsx
@@ -11,15 +11,17 @@ import { useShoppingCart } from "../../hooks/useShoppingCart"
 import { useCartItem } from "../../hooks/useCartItem"
 import { useFavorite } from "../../hooks/useFavorite"
 import { useState } from "react"
+import useEmblaCarousel from "embla-carousel-react"
 
 const ProductCard = (props: IProduct) => {
   const { handleAddToCart, handleDecrement } = useShoppingCart()
-  
-  const {isFavorite, toggleFavorite} = useFavorite(props)
+
+  const { isFavorite, toggleFavorite } = useFavorite(props)
   const [selectedColor, setSelectedColor] = useState<string | undefined>(
     props.product_colors.length ? props.product_colors[0].hex_value : undefined
   )
   const { quantity } = useCartItem(props, selectedColor)
+  const [emblaRef] = useEmblaCarousel({ axis: "x", dragFree: true })
 
   return (
     <div className="w-full bg-white border border-gray-300 duration-500 hover:scale-105 shadow-md">
@@ -38,6 +40,7 @@ const ProductCard = (props: IProduct) => {
           )}
         </button>
       </div>
+
       <div className="flex justify-center pb-5">
         <Link to={`/product-page?id=${props.id}`}>
           <img
@@ -55,26 +58,31 @@ const ProductCard = (props: IProduct) => {
         <p className="font-sans font-normal text-md text-left text-gray-800 truncate block capitalize">
           {removeFirstWord(props.name, props.brand)}
         </p>
-        <div className="flex justify-between items-center">
-          <p className="font-serif 7text-md font-bold text-gray-800 cursor-auto my-3">
-            {props.price_sign ? props.price_sign : "$"}
-            {props.price}
-          </p>
 
-          {props.product_colors.length > 1 && (
-            <div className="flex overflow-x-auto py-5">
+        {props.product_colors.length > 1 && (
+          <div className="overflow-hidden w-full" ref={emblaRef}>
+            <div className="flex space-x-2">
               {props.product_colors.map((color, index) => (
                 <div
                   key={index}
-                  className={`w-6 h-6 rounded-full border border-gray-300 ${
-                    selectedColor === color.hex_value ? "ring-2 ring-accent" : ""
+                  className={`w-5 h-5 mt-2 rounded-full border border-gray-300 flex-shrink-0 ${
+                    selectedColor === color.hex_value
+                      ? "ring-2 ring-accent border-gray-600"
+                      : ""
                   }`}
                   style={{ backgroundColor: color.hex_value }}
                   onClick={() => setSelectedColor(color.hex_value)}
                 ></div>
               ))}
             </div>
-          )}
+          </div>
+        )}
+
+        <div className="flex justify-between items-center">
+          <p className="font-serif 7text-md font-bold text-gray-800 cursor-auto my-3">
+            {props.price_sign ? props.price_sign : "$"}
+            {props.price}
+          </p>
 
           {quantity > 0 ? (
             <div className="flex gap-1 text-md items-end">
@@ -88,7 +96,9 @@ const ProductCard = (props: IProduct) => {
                 </button>
                 <p>{quantity}</p>
               </div>
-              <button onClick={() => handleAddToCart(props, selectedColor)}>+</button>
+              <button onClick={() => handleAddToCart(props, selectedColor)}>
+                +
+              </button>
             </div>
           ) : (
             <button onClick={() => handleAddToCart(props, selectedColor)}>
