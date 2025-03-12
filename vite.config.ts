10dbb32f044f82d5528969b14380dee9541f26e5
Ida Krech <krech.ida@gmail.com>
2025-03-12 21:57:20 +0100

    Revert to http instead of https

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
+// import fs from 'fs'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 
-const sslCertPath = process.env.SSL_CERT_PATH
-const sslKeyPath = process.env.SSL_KEY_PATH
+// const sslCertPath = process.env.SSL_CERT_PATH
+// const sslKeyPath = process.env.SSL_KEY_PATH
 
-if (!sslCertPath || !sslKeyPath) {
-  throw new Error("SSL certificate and key paths must be defined in .env")
-}
+// if (!sslCertPath || !sslKeyPath) {
+//   throw new Error("SSL certificate and key paths must be defined in .env")
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(sslKeyPath),
-      cert: fs.readFileSync(sslCertPath),
-    }
-  }
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync(sslKeyPath),
+  //     cert: fs.readFileSync(sslCertPath),
+  //   }
+  // }
 })
