5c327cf1fc78de7c7e725ad6c58bfdf3c1b519f3
Ida Krech <krech.ida@gmail.com>
2025-03-14 17:10:25 +0100

    Disable payment if empty card form or invalid card numbers

--- src/components/checkout/CardPayment.tsx
+++ src/components/checkout/CardPayment.tsx
@@ -6,6 +6,8 @@ const CardPayment = () => {
   const stripe = useStripe()
   const elements = useElements()
   const [paymentStatus, setPaymentStatus] = useState<string | null>(null)
+  const [cardError, setCardError] = useState<string | null>(null)
+  const [isCardComplete, setIsCardComplete] = useState<boolean>(false)
   const { createTransaction} = useTransaction()
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -20,17 +22,33 @@ const CardPayment = () => {
     }, 2000)
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleCardChange = (event: any) => {
+    if (event.complete) {
+      setIsCardComplete(true)
+      setCardError(null)
+    } else if (event.error) {
+      setCardError(event.error.message)
+      setIsCardComplete(false)
+    } else {
+      setIsCardComplete(false)
+    }
+  }
+
   return (
     <div>
-      <form onSubmit={handleSubmit} className="">
+      <form onSubmit={handleSubmit}>
         <div className="border border-zinc-300 py-1 px-2 w-1/2">
-          <CardElement options={{ hidePostalCode: true }} />
+          <CardElement 
+          options={{ hidePostalCode: true }} 
+          onChange={handleCardChange}
+          />
         </div>
 
         <div className="flex justify-center w-full">
           <button
             type="submit"
-            disabled={!stripe}
+            disabled={!stripe || !!cardError || !isCardComplete}
             className="border border-zinc-300 text-center text-zinc-700 p-3 mt-6 bg-accent hover:bg-dark rounded-md duration-200 ease-in w-1/3 font-bold font-lg"
           >
             Pay Now
