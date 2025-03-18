3bf87b4ac485f771db73fb315d68d6ebee1aeb28
Ida Krech <krech.ida@gmail.com>
2025-03-18 17:57:05 +0100

    Move favorite button position onto image

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -33,17 +33,36 @@ const ProductPage = () => {
       : undefined
   )
   const cart = useSelector((state: AppState) => state.cart.products)
-  const { quantity } = cart.find((item) => item.product.id === product?.id && item.selectedColor === selectedColor) || { quantity: 0 }
+  const { quantity } = cart.find(
+    (item) =>
+      item.product.id === product?.id && item.selectedColor === selectedColor
+  ) || { quantity: 0 }
 
   return (
     <div>
       {product && (
         <div className="flex py-5">
-          <div className="flex w-1/2 justify-center items-center">
+          <div className="flex w-1/2 justify-center items-center relative">
             <img
               src={product.image_link}
               className="w-full border border-slate-200"
             />
+            <button
+              onClick={() => toggleFavorite()}
+              className="absolute top-2 right-2 bg-white/75 rounded-full p-1"
+            >
+              {!isFavorite ? (
+                <FavoriteBorderOutlined
+                  fontSize="large"
+                  sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+                />
+              ) : (
+                <FavoriteOutlined
+                  fontSize="large"
+                  sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+                />
+              )}
+            </button>
           </div>
 
           <div className="w-1/2 text-left py-2 px-5">
@@ -74,16 +93,18 @@ const ProductPage = () => {
               </div>
             )}
 
-            <p className="py-5">{formatDescription(product.description)}</p>
+            <p className="pt-5 pb-2">{formatDescription(product.description)}</p>
 
             {product.product_colors.length > 0 && (
-              <div className="flex items-center gap-2 py-5">
+              <div className="flex items-center gap-2 py-3">
                 {product.product_colors.map((color, index) => (
                   <Tooltip title={color.colour_name}>
                     <div
                       key={index}
                       className={`w-6 h-6 rounded-full border border-gray-300 ${
-                        selectedColor === color.hex_value ? "ring-2 ring-accent" : ""
+                        selectedColor === color.hex_value
+                          ? "ring-2 ring-accent"
+                          : ""
                       }`}
                       style={{ backgroundColor: color.hex_value }}
                       onClick={() => setSelectedColor(color.hex_value)}
@@ -93,27 +114,14 @@ const ProductPage = () => {
               </div>
             )}
 
-            <div className="flex">
-              <button onClick={() => toggleFavorite()}>
-                {!isFavorite ? (
-                  <FavoriteBorderOutlined
-                    fontSize="small"
-                    sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-                  />
-                ) : (
-                  <FavoriteOutlined
-                    fontSize="small"
-                    sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
-                  />
-                )}
-              </button>
+            <div className="flex py-3">
               {quantity > 0 ? (
                 <div className="flex gap-1 text-md items-end bg-accent/50 py-2 px-4 rounded-md justify-center items-center">
                   <ShoppingCart
                     fontSize="small"
                     sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
                   />
-                  <button onClick={() => handleDecrement(product)}>
+                  <button onClick={() => handleDecrement(product, selectedColor)}>
                     <div className="rounded-full border border-zinc-300 bg-accent w-4 h-4 flex items-center justify-center pb-[3px] ml-2 mr-1">
                       -
                     </div>
