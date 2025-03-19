e2d1f9841de1a595d0f87724a797d0132465de7b
Ida Krech <krech.ida@gmail.com>
2025-03-19 18:34:40 +0100

    Display user-friendly message on data retrieving error

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -10,41 +10,50 @@ const UserPage = () => {
   const { tab } = useParams<{ tab: string }>()
   const favorites = useSelector((state: AppState) => state.favorites.products)
   const user = useSelector((state: AppState) => state.auth.user)
-  const { loading } = useUserData()
+  const { loading, error } = useUserData()
   return (
     <div className="w-full flex justify-center">
       {user ? (
-        <>
-          {tab === "account-details" && (
-            <div className="bg-white p-4 w-2/3 border border-zinc-300 my-4">
-              <h3 className="text-xl text-center font-serif font-bold mx-3 mt-2 border-b border-zinc-300 pb-1">
-                Account details
-              </h3>
-              {loading ? (
-                <p>Loading your account information...</p>
-              ) : (
-                <div className="mx-3 my-4">
-                  <AddressForm />
-                </div>
-              )}
-            </div>
-          )}
-          {tab === "order-history" && <OrderHistory />}
-          {tab === "favorites" && (
-            <ProductGrid
-              products={favorites}
-              isExpanded={false}
-              title="Favorites"
-            />
-          )}
-        </>
+        error ? (
+          <div className="bg-white p-4 w-full border border-zinc-300 my-4">
+            <h3 className="font-serif font-semibold text-center">
+              Sorry, we cannot access your personal information at the moment due to an error. Please try again later 💕
+            </h3>
+          </div>
+        ) : (
+          <>
+            {tab === "account-details" && (
+              <div className="bg-white p-4 w-2/3 border border-zinc-300 my-4">
+                <h3 className="text-xl text-center font-serif font-bold mx-3 mt-2 border-b border-zinc-300 pb-1">
+                  Account details
+                </h3>
+                {loading ? (
+                  <p>Loading your account information...</p>
+                ) : (
+                  <div className="mx-3 my-4">
+                    <AddressForm />
+                  </div>
+                )}
+              </div>
+            )}
+            {tab === "order-history" && <OrderHistory />}
+            {tab === "favorites" && (
+              <ProductGrid
+                products={favorites}
+                isExpanded={false}
+                title="Favorites"
+              />
+            )}
+          </>
+        )
       ) : (
         <div className="bg-white p-4 w-2/3 border border-zinc-300 my-4">
           <h3 className="text-xl text-center font-serif font-bold mx-3 mt-2 border-b border-zinc-300 pb-1">
             Account details
           </h3>
           <p className="mt-5 mb-3 mx-3 text-center">
-            Log in or create an account for a personalized shopping experience! ✨
+            Log in or create an account for a personalized shopping experience!
+            ✨
           </p>
         </div>
       )}
