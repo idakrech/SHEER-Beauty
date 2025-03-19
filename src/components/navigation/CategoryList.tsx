a2b471ecaaf5379d2b65f0fabe3bf1680cde61c5
Ida Krech <krech.ida@gmail.com>
2025-03-19 19:16:05 +0100

    Clean up code

--- src/components/navigation/CategoryList.tsx
+++ src/components/navigation/CategoryList.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Link } from "react-router-dom"
 import { productTypes } from "../../constants/productTypes"
 import { formatParamName } from "../../helpers/formatParamNames"
