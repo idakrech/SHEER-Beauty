7982a1979b39a97bb8902befff78268531cab08f
Ida Krech <krech.ida@gmail.com>
2025-03-21 12:40:27 +0100

    Update style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 const Button = ({ onClick, children }: { onClick: () => void; children: React.ReactNode }) => {
   return (
-    <button onClick={onClick} className="h-12 w-auto px-4 py-2 border border-zinc-500 font-sans font-semibold text-md text-gray-500 bg-primary hover:bg-accent/50">
+    <button onClick={onClick} className="h-12 w-auto px-4 py-2 border border-zinc-500 rounded-md font-sans font-semibold text-md bg-accent/50 hover:bg-accent">
     {children}
   </button>
   )
