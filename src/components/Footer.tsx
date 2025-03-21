79339079d0ea0631d86fcf5c08db5169328cbadb
Ida Krech <krech.ida@gmail.com>
2025-03-21 11:29:18 +0100

    Create footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import insta from "../assets/instagram.png"
+import fb from "../assets/facebook.png"
+import x from "../assets/x.png"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="flex items-center justify-center bg-accent footer">
+      <div className="w-2/3">
+        <div className="w-full grid grid-cols-4 gap-2 py-8">
+          <div className="flex flex-col">
+            <a href="/about-us">About Us</a>
+            <a href="/careers">Careers</a>
+            <a href="/blog">Blog</a>
+          </div>
+
+          <div className="flex flex-col">
+            <a href="/contact-us">Contact Us</a>
+            <a href="/returns-and-exchanges">Returns & Exchanges</a>
+            <a href="/faq">FAQ</a>
+            <a href="/shipping-information">Shipping Information</a>
+          </div>
+
+          <div className="flex flex-col">
+            <a href="/privacy-policy">Privacy Policy</a>
+            <a href="/terms-and-conditions">Terms & Conditions</a>
+            <a href="/gift-cards">Gift Cards</a>
+          </div>
+
+          <div className="flex gap-2 items-start justify-start">
+            <a href="https://instagram.com"><img src={insta} className="w-8"/></a>
+            <a href="https://facebook.com"><img src={fb} className="w-8"/></a>
+            <a href="https://x.com"><img src={x} className="w-8"/></a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
