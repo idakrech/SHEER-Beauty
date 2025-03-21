37d66070bd62b1fb73ccf679e6a888be6b9b306c
Ida Krech <krech.ida@gmail.com>
2025-03-21 12:58:39 +0100

    Use Big.js to calculate price subtotal

--- src/hooks/useShoppingCart.ts
+++ src/hooks/useShoppingCart.ts
@@ -8,6 +8,7 @@ import {
 } from "../redux/cartSlice"
 import { userDataService } from "../services/userDataService"
 import { useEffect, useState } from "react"
+import Big from "big.js"
 
 export function useShoppingCart() {
   const user = useSelector((state: AppState) => state.auth.user)
@@ -19,41 +20,65 @@ export function useShoppingCart() {
 
   useEffect(() => {
     setPriceSum(
-      (cart.products ?? []).reduce((sum, { product, quantity }) => sum + parseFloat(product.price) * quantity, 0)
+      (cart.products ?? [])
+        .reduce(
+          (sum, { product, quantity }) =>
+            sum.plus(Big(product.price).times(quantity)),
+          Big(0)
+        )
+        .toNumber()
     )
   }, [cart])
 
   useEffect(() => {
-    setProductsQuantity(cart.products.reduce((total, product) => total + product.quantity, 0))
+    setProductsQuantity(
+      cart.products.reduce((total, product) => total + product.quantity, 0)
+    )
   }, [cart])
 
   const handleDelete = (product: IProduct, selectedColor?: string) => {
-    dispatch(deleteProduct({product, selectedColor}))
+    dispatch(deleteProduct({ product, selectedColor }))
     if (user !== null) {
       userDataService.removeFromCart(user.uid, product, selectedColor)
     }
   }
 
   const handleDecrement = (product: IProduct, selectedColor?: string) => {
-    dispatch(decrementProductQuantity({product, selectedColor}))
+    dispatch(decrementProductQuantity({ product, selectedColor }))
     if (user !== null) {
       userDataService.decrementProductQuantity(user.uid, product, selectedColor)
     }
   }
 
   const handleAddToCart = (product: IProduct, selectedColor?: string) => {
-    dispatch(addProduct({product, selectedColor}))
+    dispatch(addProduct({ product, selectedColor }))
     if (user !== null) {
-      userDataService.addToCart(user.uid, { product, quantity: 1, selectedColor })
+      userDataService.addToCart(user.uid, {
+        product,
+        quantity: 1,
+        selectedColor,
+      })
     }
   }
 
-  const getColorName = (product: IProduct, selectedColor?: string): string | null => {
+  const getColorName = (
+    product: IProduct,
+    selectedColor?: string
+  ): string | null => {
     if (!selectedColor) return null
-    const foundColor = product.product_colors.find(color => color.hex_value === selectedColor)
+    const foundColor = product.product_colors.find(
+      (color) => color.hex_value === selectedColor
+    )
     return foundColor ? foundColor.colour_name : null
   }
 
-
-  return { cartProducts, handleDelete, handleDecrement, handleAddToCart, priceSum, productsQuantity, getColorName }
+  return {
+    cartProducts,
+    handleDelete,
+    handleDecrement,
+    handleAddToCart,
+    priceSum,
+    productsQuantity,
+    getColorName,
+  }
 }
