6a7f01751de0e14afdf9d71657e5abb270793db4
Ida Krech <krech.ida@gmail.com>
2025-03-23 18:53:18 +0100

    Remove img credits

--- src/config/gridConfig.ts
+++ src/config/gridConfig.ts
@@ -1,14 +1,4 @@
-import cloudImg from "../assets/grid-images/cloud-woman.jpg"
-import blushImg from "../assets/grid-images/model-blush.jpg"
-import gradient1 from "../assets/gradients/v904-nunny-029-g.jpg"
-import gradient2 from "../assets/gradients/v904-nunny-015-l.jpg"
-import gradient3 from "../assets/gradients/v904-nunny-035-l.jpg"
-import gradient4 from "../assets/gradients/v904-nunny-005-l.jpg"
-import blushCheeks2 from "../assets/grid-images/blush-cheeks-2.jpg"
-import faceFlower from "../assets/grid-images/face-flower.jpg"
-
 import blush from "../assets/grid-images/blush.jpg"
-import candy from "../assets/grid-images/candy.jpg"
 import mascara from "../assets/grid-images/mascara.jpg"
 import smiley from "../assets/grid-images/smiley.jpg"
 
@@ -58,21 +48,8 @@ export const gridConfig: IGridConfig[] = [
   ]
 
   export const imageMap: Record<string, string> = {
-    "cloud-woman.jpg": cloudImg,
-    "model-blush.jpg": blushImg,
-    "v904-nunny-029-g.jpg": gradient1,
-    "v904-nunny-015-l.jpg": gradient2,
-    "v904-nunny-035-l.jpg": gradient3,
-    "v904-nunny-005-l.jpg": gradient4,
-    "blush-cheeks-2.jpg": blushCheeks2,
-    "face-flower.jpg": faceFlower,
     "blush.jpg": blush,
-    "candy.jpg": candy,
     "mascara.jpg": mascara,
     "smiley.jpg": smiley
   }
   
-  //<a href="https://www.freepik.com/free-photo/flirty-dark-haired-girl-pink-headband-massive-earrings-covers-eye-with-candy_12727462.htm">Image by lookstudio on Freepik</a>
-  //<a href="https://www.freepik.com/free-photo/beautiful-young-woman-portrait-with-make-up-product_17861190.htm">Image by freepik</a>
-  //<a href="https://www.freepik.com/free-photo/high-angle-beautiful-woman-wearing-pink-makeup_25967301.htm">Image by freepik</a>
-  //<a href="https://www.freepik.com/free-photo/side-view-smiley-woman-posing-with-pink-background_25967304.htm">Image by freepik</a>
