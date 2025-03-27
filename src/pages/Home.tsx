f2874230c824febf516e92d69a397fd6a38fb3a1
Ida Krech <krech.ida@gmail.com>
2025-03-27 11:54:04 +0100

    Display website error in case of no fetched products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { IFilterState } from "../redux/filterSlice"
 import { useSelector } from "react-redux"
 import { AppState } from "../redux"
 import { useGridProducts } from "../hooks/useGridProducts"
+import WebsiteError from "../components/WebsiteError"
 
 const Home = () => {
   const isInitialized = useSelector(
@@ -12,8 +13,16 @@ const Home = () => {
 
   const gridProducts = useGridProducts(isInitialized)
 
+  const allEmpty = Object.values(gridProducts).every(
+    (products) => products.length === 0
+  )
+
+  if (allEmpty) {
+    return <WebsiteError/>
+  }
+
   return (
-    <div className="max-w-screen-xl mx-auto">
+    <div className="max-w-screen-xl mx-auto -mt-2 md:mt-0">
       {gridConfig.map((grid) => {
         const products = gridProducts[grid.title] || []
         const filters: IFilterState = {
@@ -28,19 +37,19 @@ const Home = () => {
         return (
           <div key={grid.title}>
             <div
-              className="w-full h-[50vh] sm:h-[40vh] md:h-[50vh] lg:h-96 flex relative bg-cover bg-center bg-no-repeat items-center justify-center border border-zinc-300"
+              className="w-full h-[50vh] lg:h-96 flex relative bg-cover bg-center bg-no-repeat items-center justify-center"
               style={{ backgroundImage: `url(${imageMap[grid.image]})` }}
             >
               <div className="absolute w-full h-full z-10 bg-gradient-to-t from-accent to-transparent"></div>
 
-              <div className="w-auto h-auto flex flex-col justify-center items-center z-20 gap-0 p-4 sm:p-6 md:p-10">
-                <h3 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-primary italic font-serif">
+              <div className="w-auto h-auto flex flex-col justify-center items-center z-20 gap-0 p-6 md:p-10">
+                <h3 className="text-4xl md:text-6xl lg:text-7xl text-primary italic font-serif">
                   {grid.title}
                 </h3>
-                <h3 className="text-sm sm:text-base md:text-lg lg:text-xl uppercase font-extralight text-primary tracking-wider">{grid.subtitle}</h3>
+                <h3 className="text-sm md:text-lg lg:text-xl uppercase font-extralight text-primary tracking-wider">{grid.subtitle}</h3>
               </div>
             </div>
-            <div key={grid.title} className="bg-primary p-2 sm:p-4 md:p-6">
+            <div key={grid.title} className="bg-primary p-4 md:p-6">
               <ProductGrid
                 products={products}
                 isExpanded={false}
