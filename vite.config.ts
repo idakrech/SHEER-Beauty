ad842a33f7ebf9c1e7c0ae7bb1c753d5fbf6ff6f
Ida Krech <krech.ida@gmail.com>
2025-03-27 17:21:28 +0100

    Hide https config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import fs from 'fs'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
 
 
-const sslCertPath = process.env.SSL_CERT_PATH
-const sslKeyPath = process.env.SSL_KEY_PATH
+// const sslCertPath = process.env.SSL_CERT_PATH
+// const sslKeyPath = process.env.SSL_KEY_PATH
 
-if (!sslCertPath || !sslKeyPath) {
-  throw new Error("SSL certificate and key paths must be defined in .env")
-}
+// if (!sslCertPath || !sslKeyPath) {
+//   throw new Error("SSL certificate and key paths must be defined in .env")
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
